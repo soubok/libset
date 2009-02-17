dcc75ac389fa7efb07aebfbb2abebb46d1004df9
soubok <soubok@891555b4-9a40-0410-9c19-e1418fb94df6>
2009-02-17 13:49:37 +0000

    - update to the latest TraceMonkey version from mozilla-central-4c6d799751ff

--- spidermonkey/src/jsdbgapi.cpp
+++ spidermonkey/src/jsdbgapi.cpp
@@ -651,9 +651,10 @@ js_watch_set(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
 #endif
                 ok = !wp->setter ||
                      ((sprop->attrs & JSPROP_SETTER)
-                      ? js_InternalCall(cx, obj, OBJECT_TO_JSVAL(wp->setter),
+                      ? js_InternalCall(cx, obj,
+                                        OBJECT_TO_JSVAL((JSObject *)wp->setter),
                                         1, vp, vp)
-                      : wp->setter(cx, OBJ_THIS_OBJECT(cx, obj), userid, vp));
+                      : wp->setter(cx, obj, userid, vp));
                 if (injectFrame) {
                     /* Evil code can cause us to have an arguments object. */
                     if (frame.callobj)
